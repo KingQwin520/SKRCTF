6b39d443f027bd53a4b8b8cbdac3cdfdba53568f
KingQwin <79892065+KingQwin520@users.noreply.github.com>
2022-06-19 21:08:01 +0800

    Create index.php

---
+++ Web/Admin Dashboard/index.php
@@ -0,0 +1,46 @@
+<script>window.location.href='login.php'</script><html>
+
+<head>
+        <title>Admin Page</title>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.js" integrity="sha256-BTlTdQO9/fascB1drekrDVkaKd9PkwBymMlHOiG+qLI=" crossorigin="anonymous"></script>
+        <!-- Bootstrap core CSS -->
+        <link href="./bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div class="container">
+                <a class="navbar-brand" href="index.php">Admin Dashboard</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                        <a class="nav-link" href="index.php">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                        <a class="nav-link" href="logs.php">Logs</a>
+                                </li>
+                                <!--
+                                <li class="nav-item">
+                                        <a class="nav-link" href="S3cr3t_P4g3.php">Flag</a>
+                                </li>
+                                -->
+                        </ul>
+                </div>
+        </div>
+</nav>
+
+<main role="main" class="container">
+        <h1 class="mt-5">Welcome Admin</h1>
+        <p class="lead">Updated version 2.0</p>
+        <ul>
+                <li>Register page is disabled</li>
+                <li>Flag page is currently under maintenance</li>
+        </ul>
+</main>
+
+</body>
+
+</html>
